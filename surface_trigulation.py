b135afbe8418ee7a602141441981a44056248d46
Rohit Rathod <83444956+rohit-rathod-13@users.noreply.github.com>
2021-12-07 12:03:40 +0530

    Create surface_trigulation.py

---
+++ surface_trigulation.py
@@ -0,0 +1,15 @@
+#Surface Triangulation
+from mpl_toolkits import mplot3d
+
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+
+theta = 2 * np.pi * np.random.random(1000)
+r = 6 * np.random.random(1000)
+x = np.ravel(r * np.sin(theta))
+y = np.ravel(r * np.cos(theta))
+z = f(x, y)
+
+ax = plt.axes(projection='3d')
+ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=0.5);
